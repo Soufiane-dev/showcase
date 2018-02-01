0d8cf5a5a840915676933652558b06a436d84919
Soufiane Bamou <soufiane.sd@iicloud.com>
2018-02-01 15:33:16 +0000

    tryna fix an error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser     = require('body-parser');
 const app            = express();
 const db             = require('./config/db');
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,7 +13,7 @@ MongoClient.connect(db.url, (err, database) => {
 
   if(err) return console.log(err)
   require('./app/routes')(app, database);
-  server.listen(port, function() {
-    console.log("App is running on port " + port);
-});
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
 })
