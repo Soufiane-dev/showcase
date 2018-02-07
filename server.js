c2cc7177035410ac24c0f3c6a4aee65dad1a933c
Soufiane Bamou <soufiane.sd@iicloud.com>
2018-02-07 17:00:35 +0000

    new log

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ MongoClient.connect(db.url, (err, database) => {
   if(err) return console.log(err)
   require('./app/routes')(app, database);
   app.listen(port, () => {
+    console.log("Welcome to ScreenDy V3 Showcase API");
     console.log('We are live on ' + port);
   });
 })
